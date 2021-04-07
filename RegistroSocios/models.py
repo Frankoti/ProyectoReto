46f4eb5c10f1d6cad2c3dbcb00fa866f50b66905
Frankoti <sfcotrina@est.itsgg.edu.ec>
2021-04-07 00:17:42 -0500

    modelos

--- RegistroSocios/models.py
+++ RegistroSocios/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+
+class Puesto(models.Model):
+    idpuesto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = "puesto"
+        verbose_name = "puesto"
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    idcategoria = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = "categoria"
+        verbose_name = "categoria"
+
+    def __str__(self):
+        return self.nombre
+
+class Persona(models.Model):
+    idpersona = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    genero = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=30)
+    telefono= models.IntegerField()
+    condicion_civil= models.CharField(max_length=30)
+    puesto_idpuesto= models.ForeignKey(Puesto, on_delete=models.CASCADE, db_column= "idpuesto")
+    categoria_idcategoria= models.ForeignKey(Categoria, on_delete=models.CASCADE, db_column="idcascade")
+
+    class Meta:
+        db_table = "persona"
+        verbose_name = "persona"
+
+class Registro(models.Model):
+    idregistro = models.AutoField(primary_key=True)
+    fecha = models.DateTimeField()
+    fotoQR= models.ImageField()
+    fotorostro = models.ImageField()
+    persona_idpersona = models.ForeignKey(Persona, on_delete=models.CASCADE, db_column="idpersona")
+
+    class Meta:
+        db_table = "registro"
+        verbose_name = "registro"
+
+
+
