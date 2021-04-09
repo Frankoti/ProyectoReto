c214fe35c26bc243d5f590312540580498127c1b
Frankoti <sfcotrina@est.itsgg.edu.ec>
2021-04-09 14:43:55 -0500

    cambio pychart

--- RegistroSocios/credencial/codigo.py
+++ RegistroSocios/credencial/codigo.py
@@ -1,7 +1,7 @@
 import qrcode 
 
--
-imagen = qrcode.make('0914984539') 
-archivo_imagen = open('codigo.png', 'wb')
+
+imagen = qrcode.make('0914984521') 
+archivo_imagen = open('codigo.jpg', 'wb')
 imagen.save(archivo_imagen)
 archivo_imagen.close()
